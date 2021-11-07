56cc77e4188ec0ac3746ac0889266585b693d3ff
saad <saadh9290@gmail.com>
2021-11-07 13:45:31 +0500

    intent activity

--- app/src/main/java/com/example/lec09_10/MainActivity.java
+++ app/src/main/java/com/example/lec09_10/MainActivity.java
@@ -16,55 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn= findViewById(R.id.button);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                CallingIntent();
-//            }
-//        });
-//
-
-    }
-    public void CallingIntent() {
-
-        Uri uri = Uri.parse("tel:+923001234567");
-
-        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
-
-        startActivity(intent);
-
-    }
-    public void onClick(View view){
-        Intent intent;
-
-            switch (view.getId()) {
-
-                case
-                        R.id.button:
-
-                    intent = new
-                            Intent(MainActivity.this,
-                            MainActivity2.class);
-
-                    startActivity(intent);
-
-                    break;
-
-                case R.id.button2:
-
-                    Uri uri =
-                            Uri.parse("tel:+923001234567");
-
-                    intent = new
-                            Intent(Intent.ACTION_DIAL, uri);
-
-                    startActivity(intent);
-
-                    break;
 
 
-            }
 
     }
 }
